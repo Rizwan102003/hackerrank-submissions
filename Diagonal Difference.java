feea819f6523e3cb8165a0c99bc8946fe96c8465
Rizwan <skmdrizwan2003@gmail.com>
2025-08-05 01:01:54 +0530

    news

--- Diagonal Difference.java
+++ Diagonal Difference.java
@@ -7,3 +7,17 @@ public static int diagonalDifference(List<List<Integer>> arr) {
     }
     return Math.abs(ld - rd);
 }
+/* O(n) approach
+int ld=0,rd=0;
+int rows=arr.length;
+int cols=arr[0].length;
+int i=0,j=0,k=0,l=arr.length-1;
+while(i<rows && j<cols && k<rows && i>=0 && l>=0){
+    ld+=arr[i][j];
+    rd+=arr[k][l];
+    i++;
+    j++;
+    k++;
+    l--;
+}
+*/
